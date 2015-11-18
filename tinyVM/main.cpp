525d4c6cb5ed00f542cdcb165efd61e58e72bb29
Freakmiko <freakmiki@gmail.com>
2015-11-18 23:32:13 +0100

    removed fibonacci from main.cpp

--- tinyVM/main.cpp
+++ tinyVM/main.cpp
@@ -12,12 +12,3 @@ int main(int argc, char* argv[])
 //	std::cout << fib(6) << std::endl;
 	return 0;
 }
-
-int fib(int n) {
-	if (n == 0)
-		return 0;
-	if (n == 1)
-		return 1;
-	else
-		return fib(n-1) + fib(n-2);
-}
